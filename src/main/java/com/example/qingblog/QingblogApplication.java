d3f6e2b34c25f1c1e50137a2ec0400532d3563c6
bighansome <948084447@qq.com>
2018-06-01 09:48:10 +0800

    init

---
+++ src/main/java/com/example/qingblog/QingblogApplication.java
@@ -0,0 +1,12 @@
+package com.example.qingblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingblogApplication.class, args);
+    }
+}
